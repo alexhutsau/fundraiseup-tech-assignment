90f78c31a8f16ee182988cab62b9070a8b719cb6
Alex Hutsau <aleksan9ep@gmail.com>
2023-07-23 20:32:33 +0200

    Implement sync.ts

--- src/db.ts
+++ src/db.ts
@@ -6,13 +6,16 @@ if (!DB_URI) {
   throw new Error("DB_URI env is missed");
 }
 
-export const mongoClient = new MongoClient(DB_URI);
+export const mongoClient = new MongoClient(DB_URI, {
+  // directConnection: true, // temporary replica set mode fix for "MongoNetworkError: getaddrinfo ENOTFOUND"
+});
 
 export async function initConnection(): Promise<MongoClient> {
   return mongoClient.connect();
 }
 
 function closeConnection(): void {
+  // "MongoNotConnectedError: Client must be connected before running operations" when using watch streams
   mongoClient.close();
 }
 
--- src/sync.ts
+++ src/sync.ts
@@ -2,8 +2,22 @@ import "dotenv/config";
 
 import { initConnection, mongoClient } from "./db";
 import { anonymiseCustomer } from "./utils";
+import { ChangeStreamInsertDocument, Timestamp, WithId } from "mongodb";
+import { Customer } from "./interfaces";
 
 const isFullReindexMode = process.argv.includes("--full-reindex");
+
+async function saveAnonymisedCustomers(
+  customers: WithId<Customer>[],
+): Promise<void> {
+  await mongoClient
+    .db("fundraiseup")
+    .collection("customers_anonymised")
+    .insertMany(customers, { ordered: false })
+    .catch((error) => {
+      if (error.code !== 11000) throw error;
+    });
+}
 
 async function runFullReindex(): Promise<void> {
   const limit = 1000;
@@ -12,7 +26,7 @@ async function runFullReindex(): Promise<void> {
   while (true) {
     const customers = await mongoClient
       .db("fundraiseup")
-      .collection("customers")
+      .collection<Customer>("customers")
       .find()
       .sort("_id", 1)
       .limit(limit)
@@ -21,13 +35,7 @@ async function runFullReindex(): Promise<void> {
 
     if (!customers.length) break;
 
-    await mongoClient
-      .db("fundraiseup")
-      .collection("customers_anonymised")
-      .insertMany(customers.map(anonymiseCustomer), { ordered: false })
-      .catch((error) => {
-        if (error.code !== 11000) throw error;
-      });
+    await saveAnonymisedCustomers(customers.map(anonymiseCustomer));
 
     page++;
   }
@@ -39,5 +47,61 @@ async function runFullReindex(): Promise<void> {
   if (isFullReindexMode) {
     await runFullReindex();
     process.exit();
+  }
+
+  const [lastAnonymised] = await mongoClient
+    .db("fundraiseup")
+    .collection<Customer>("customers_anonymised")
+    .find()
+    .sort("_id", -1)
+    .limit(1)
+    .toArray();
+
+  const firstCreated =
+    !lastAnonymised &&
+    (await mongoClient
+      .db("fundraiseup")
+      .collection<Customer>("customers")
+      .findOne());
+
+  const startAtOperationTime = (lastAnonymised || firstCreated)?.createdAt;
+
+  const watchStream = mongoClient
+    .db("fundraiseup")
+    .collection("customers")
+    .watch([{ $match: { operationType: "insert" } }], {
+      ...(startAtOperationTime && {
+        startAtOperationTime: new Timestamp({
+          t: Math.floor(+startAtOperationTime / 1000),
+          i: 1,
+        }),
+      }),
+    });
+
+  const anonymisedCustomers: WithId<Customer>[] = [];
+
+  setInterval(async (): Promise<void> => {
+    const len = anonymisedCustomers.length;
+
+    if (len) {
+      console.log(`saving ${anonymisedCustomers.length} by interval...`);
+      await saveAnonymisedCustomers(anonymisedCustomers.splice(0, len));
+    }
+  }, 1000);
+
+  console.log(`start from ${startAtOperationTime}`);
+
+  while (true) {
+    const { fullDocument: customer } =
+      (await watchStream.next()) as ChangeStreamInsertDocument<
+        WithId<Customer>
+      >;
+
+    const len = anonymisedCustomers.push(anonymiseCustomer(customer));
+
+    if (len >= 1000) {
+      console.log(`saving ${len} by size...`);
+      await saveAnonymisedCustomers(anonymisedCustomers.splice(0, len));
+    }
   }
 })();
